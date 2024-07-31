979efa49ff093332ef1a54d25ec7fa05af4189f5
uuqjz <nils.niehues@kit.edu>
2024-07-31 09:42:54 +0200

    BiMap, node chars

--- tests/dev.abunai.confidentiality.mitigation.tests/src/dev/abunai/confidentiality/mitigation/tests/Sat.java
+++ tests/dev.abunai.confidentiality.mitigation.tests/src/dev/abunai/confidentiality/mitigation/tests/Sat.java
@@ -1,6 +1,7 @@
 package dev.abunai.confidentiality.mitigation.tests;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.sat4j.core.VecInt;
@@ -11,30 +12,33 @@ import org.sat4j.specs.ISolver;
 import org.sat4j.specs.TimeoutException;
 
 public class Sat {
-    
+    BiMap<List<String>, Integer> nodeCharToLiteral = new BiMap<>();
+
     @Test
     public void test() throws ContradictionException, TimeoutException {
         ISolver solver = SolverFactory.newDefault();
+        for (var node : List.of("User", "Process", "DB")) {
+            var literal = solver.nextFreeVarId(true);
+            nodeCharToLiteral.put(List.of(node, "Location", "NonEU"), literal);
+            solver.registerLiteral(literal);
+        }
 
-        solver.addClause(clause(1,2));
-        solver.addClause(clause(-1,3));
-        solver.addClause(clause(-2,-3));
+        solver.addClause(clause(nodeCharToLiteral.getValue(List.of("User", "Location", "NonEU")),
+                nodeCharToLiteral.getValue(List.of("DB", "Location", "NonEU"))));
 
         IProblem problem = solver;
         while (problem.isSatisfiable()) {
             int[] model = problem.model();
             var negated = new VecInt();
             System.out.println(Arrays.toString(model));
-            for (var literal:model) {
+            for (var literal : model) {
                 negated.push(-literal);
             }
             solver.addClause(negated);
         }
-        System.out.println(solver.nextFreeVarId(false));
     }
 
-    private VecInt clause(int... literals ) {
+    private VecInt clause(int... literals) {
         return new VecInt(literals);
     }
 }
-
