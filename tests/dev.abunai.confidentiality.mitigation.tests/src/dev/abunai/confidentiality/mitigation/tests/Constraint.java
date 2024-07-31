e258d22870a08b6c85c67387956a5c2a30c57398
uuqjz <nils.niehues@kit.edu>
2024-07-31 12:30:31 +0200

    Constraint based

---
+++ tests/dev.abunai.confidentiality.mitigation.tests/src/dev/abunai/confidentiality/mitigation/tests/Constraint.java
@@ -0,0 +1,5 @@
+package dev.abunai.confidentiality.mitigation.tests;
+
+public record Constraint(boolean positive, String what, String type, String value) {
+
+}
--- tests/dev.abunai.confidentiality.mitigation.tests/src/dev/abunai/confidentiality/mitigation/tests/Sat.java
+++ tests/dev.abunai.confidentiality.mitigation.tests/src/dev/abunai/confidentiality/mitigation/tests/Sat.java
@@ -1,7 +1,7 @@
 package dev.abunai.confidentiality.mitigation.tests;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.Test;
 import org.sat4j.core.VecInt;
@@ -12,25 +12,41 @@ import org.sat4j.specs.ISolver;
 import org.sat4j.specs.TimeoutException;
 
 public class Sat {
-    BiMap<List<String>, Integer> nodeCharToLiteral = new BiMap<>();
+    BiMap<List<String>, Integer> literalMap;
 
     @Test
     public void test() throws ContradictionException, TimeoutException {
+        literalMap = new BiMap<>();
+        // if data is personal and node in non eu then data must be encrypted
+        var constraints = List.of(new Constraint(false, "Data", "Sensitivity", "Personal"), new Constraint(false, "Node", "Location", "NonEu"),
+                new Constraint(true, "Data", "Encryption", "Encrypted"));
+        
+        var nodes = List.of("User", "Process", "DB");
+
         ISolver solver = SolverFactory.newDefault();
-        for (var node : List.of("User", "Process", "DB")) {
-            var literal = solver.nextFreeVarId(true);
-            nodeCharToLiteral.put(List.of(node, "Location", "NonEU"), literal);
-            solver.registerLiteral(literal);
+        for (var node : nodes) {
+            var clause = new VecInt();
+            for(var constraint : constraints) {
+                var literal = solver.nextFreeVarId(true);
+                literalMap.put(List.of(node, constraint.what(), constraint.type(), constraint.value()), literal);
+                clause.push((constraint.positive() ? 1 : -1) * literal);
+            }
+            solver.addClause(clause);
+            //solver.addClause(clause(literalMap.getValue(List.of(node, "Node", "Location", "NonEu"))));
+            //solver.addClause(clause(literalMap.getValue(List.of(node, "Data", "Sensitivity", "Personal"))));
         }
 
-        solver.addClause(clause(nodeCharToLiteral.getValue(List.of("User", "Location", "NonEU")),
-                nodeCharToLiteral.getValue(List.of("DB", "Location", "NonEU"))));
+        
 
         IProblem problem = solver;
         while (problem.isSatisfiable()) {
             int[] model = problem.model();
             var negated = new VecInt();
-            System.out.println(Arrays.toString(model));
+            var names = IntStream.of(model)
+                    .filter(lit -> lit > 0)
+                    .mapToObj(lit -> literalMap.getKey(lit))
+                    .toList();
+            System.out.println(names);
             for (var literal : model) {
                 negated.push(-literal);
             }
