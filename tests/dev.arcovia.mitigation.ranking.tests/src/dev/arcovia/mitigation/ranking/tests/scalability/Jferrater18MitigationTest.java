32d2a86e0ba2734da8bc27ce7b09d1983d6cd5c5
uuqjz <nils.niehues@kit.edu>
2025-02-03 08:15:05 +0100

    Move common scalability functionality to superclass

--- tests/dev.arcovia.mitigation.ranking.tests/src/dev/arcovia/mitigation/ranking/tests/scalability/Jferrater18MitigationTest.java
+++ tests/dev.arcovia.mitigation.ranking.tests/src/dev/arcovia/mitigation/ranking/tests/scalability/Jferrater18MitigationTest.java
@@ -5,14 +5,10 @@ import java.util.List;
 import java.util.function.Predicate;
 
 import org.dataflowanalysis.analysis.core.AbstractVertex;
-import org.junit.jupiter.api.Test;
 
-import dev.arcovia.mitigation.ranking.MitigationStrategy;
-import dev.arcovia.mitigation.ranking.RankerType;
-import dev.arcovia.mitigation.ranking.RankingAggregationMethod;
-import dev.arcovia.mitigation.ranking.tests.MitigationTestBase;
+import dev.arcovia.mitigation.ranking.tests.ScalabilityBase;
 
-public class Jferrater18MitigationTest extends MitigationTestBase{
+public class Jferrater18MitigationTest extends ScalabilityBase{
 	
 	protected String getFolderName() {
 		return "jferrater18";
@@ -42,61 +38,4 @@ public class Jferrater18MitigationTest extends MitigationTestBase{
 		});
 		return constraints;
 	}
-	
-	@Override
-	protected RankerType getRankerType() {
-		return RankerType.LOGISTIC_REGRESSION;
-	}
-
-	@Override
-	protected RankingAggregationMethod getAggregationMethod() {
-		return RankingAggregationMethod.EXPONENTIAL_RANKS;
-	}
-
-	private void executeMitigationStrategy(MitigationStrategy strategy) {
-        deleteOldMeassurement();
-        for (int i = 0; i < MITIGATION_RUNS; i++) {
-            var startTime = System.currentTimeMillis();
-            mitigationStrategy = strategy;
-
-            if (!strategy.equals(MitigationStrategy.BRUTE_FORCE)) {
-                createTrainData();
-            }
-
-            createMitigationCandidatesAutomatically();
-            var duration = System.currentTimeMillis() - startTime;
-            storeMeassurement(duration);
-        }
-        storeMeassurementResult(seeAverageRuntime(), getFolderName() + "_" + strategy.name());
-    }
-
-    @Test
-    public void executeHalf() {
-        executeMitigationStrategy(MitigationStrategy.HALF);
-    }
-
-    @Test
-    public void executeQuarter() {
-        executeMitigationStrategy(MitigationStrategy.QUATER);
-    }
-
-    @Test
-    public void executeIncreasing() {
-        executeMitigationStrategy(MitigationStrategy.INCREASING);
-    }
-
-    @Test
-    public void executeCluster() {
-        executeMitigationStrategy(MitigationStrategy.CLUSTER);
-    }
-
-    @Test
-    public void executeFastStart() {
-        executeMitigationStrategy(MitigationStrategy.FAST_START);
-    }
-
-    @Test
-    public void executeBruteForce() {
-        executeMitigationStrategy(MitigationStrategy.BRUTE_FORCE);
-    }
 }
--- tests/dev.arcovia.mitigation.ranking.tests/src/dev/arcovia/mitigation/ranking/tests/scalability/OnlineBanking14MitigationTest.java
+++ tests/dev.arcovia.mitigation.ranking.tests/src/dev/arcovia/mitigation/ranking/tests/scalability/OnlineBanking14MitigationTest.java
@@ -5,14 +5,10 @@ import java.util.List;
 import java.util.function.Predicate;
 
 import org.dataflowanalysis.analysis.core.AbstractVertex;
-import org.junit.jupiter.api.Test;
 
-import dev.arcovia.mitigation.ranking.MitigationStrategy;
-import dev.arcovia.mitigation.ranking.RankerType;
-import dev.arcovia.mitigation.ranking.RankingAggregationMethod;
-import dev.arcovia.mitigation.ranking.tests.MitigationTestBase;
+import dev.arcovia.mitigation.ranking.tests.ScalabilityBase;
 
-public class OnlineBanking14MitigationTest extends MitigationTestBase {
+public class OnlineBanking14MitigationTest extends ScalabilityBase {
 
 	protected String getFolderName() {
 		return "OBM_14";
@@ -22,16 +18,6 @@ public class OnlineBanking14MitigationTest extends MitigationTestBase {
 		return "OBM";
 	}
 		
-	@Override
-	protected RankerType getRankerType() {
-		return RankerType.LOGISTIC_REGRESSION;
-	}
-
-	@Override
-	protected RankingAggregationMethod getAggregationMethod() {
-		return RankingAggregationMethod.EXPONENTIAL_RANKS;
-	}
-
 	protected List<Predicate<? super AbstractVertex<?>>> getConstraints() {
 		List<Predicate<? super AbstractVertex<?>>> constraints = new ArrayList<>();
 		constraints.add(it -> {
@@ -51,51 +37,4 @@ public class OnlineBanking14MitigationTest extends MitigationTestBase {
 		});
 		return constraints;
 	}
-
-	private void executeMitigationStrategy(MitigationStrategy strategy) {
-        deleteOldMeassurement();
-        for (int i = 0; i < MITIGATION_RUNS; i++) {
-            var startTime = System.currentTimeMillis();
-            mitigationStrategy = strategy;
-
-            if (!strategy.equals(MitigationStrategy.BRUTE_FORCE)) {
-                createTrainData();
-            }
-
-            createMitigationCandidatesAutomatically();
-            var duration = System.currentTimeMillis() - startTime;
-            storeMeassurement(duration);
-        }
-        storeMeassurementResult(seeAverageRuntime(), getFolderName() + "_" + strategy.name());
-    }
-
-    @Test
-    public void executeHalf() {
-        executeMitigationStrategy(MitigationStrategy.HALF);
-    }
-
-    @Test
-    public void executeQuarter() {
-        executeMitigationStrategy(MitigationStrategy.QUATER);
-    }
-
-    @Test
-    public void executeIncreasing() {
-        executeMitigationStrategy(MitigationStrategy.INCREASING);
-    }
-
-    @Test
-    public void executeCluster() {
-        executeMitigationStrategy(MitigationStrategy.CLUSTER);
-    }
-
-    @Test
-    public void executeFastStart() {
-        executeMitigationStrategy(MitigationStrategy.FAST_START);
-    }
-
-    @Test
-    public void executeBruteForce() {
-        executeMitigationStrategy(MitigationStrategy.BRUTE_FORCE);
-    }
 }
