59c4176c55a10029c53be4c4675556d980d81f2b
BenjaminArp <101420246+BenjaminArp@users.noreply.github.com>
2025-05-12 14:10:38 +0200

    Changes to model

--- tests/dev.arcovia.mitigation.sat.tests/src/dev/arcovia/mitigation/sat/tests/ICSETest.java
+++ tests/dev.arcovia.mitigation.sat.tests/src/dev/arcovia/mitigation/sat/tests/ICSETest.java
@@ -10,6 +10,7 @@ import dev.arcovia.mitigation.sat.Literal;
 import dev.arcovia.mitigation.sat.Mechanic;
 import dev.arcovia.mitigation.sat.NodeLabel;
 import org.dataflowanalysis.converter.DataFlowDiagramConverter;
+import org.dataflowanalysis.converter.WebEditorConverter;
 import org.junit.jupiter.api.Test;
 import org.sat4j.specs.ContradictionException;
 import org.sat4j.specs.TimeoutException;
@@ -23,13 +24,16 @@ public class ICSETest extends BaseTest{
     Constraint dataConstraint = new Constraint(List.of(new Literal(false, new IncomingDataLabel(new Label("Sensitivity", "Personal"))),
             new Literal(false, new NodeLabel(new Label("Location", "nonEU"))),
             new Literal(true, new IncomingDataLabel(new Label("Encryption", "Encrypted")))));
-    Constraint nodeConstraint = new Constraint(List.of(new Literal(true, new NodeLabel(new Label("Stereotype", "internal"))),
-            new Literal(false, new NodeLabel(new Label("Encryption", "Encrypted")))));
+    // internal -> local_logging
+    Constraint nodeConstraint = new Constraint(List.of(new Literal(false, new NodeLabel(new Label("Location", "internal"))),
+            new Literal(true, new NodeLabel(new Label("Stereotype", "logging")))));
     List<Constraint> constraints = List.of(dataConstraint, nodeConstraint);
 
     @Test
     public void automaticTest() throws ContradictionException, TimeoutException, IOException {
         var dfdConverter = new DataFlowDiagramConverter();
+
+        dfdConverter.storeWeb(dfdConverter.dfdToWeb( new WebEditorConverter().webToDfd(ICSE_Sat)), ICSE_Sat);
 
         var repairedDfdCosts = new Mechanic(ICSE_Sat, constraints, costs).repair();
         dfdConverter.storeWeb(dfdConverter.dfdToWeb(repairedDfdCosts), "testresults/ICSE-repaired.json");
