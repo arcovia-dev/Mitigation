949c8ee9e66c2311b8803ef444811ac4be8b240c
jensii <jens.weber@uni-ulm.de>
2025-08-15 12:44:33 +0200

    adjusted formula generation

--- bundles/dev.arcovia.mitigation.sat/src/dev/arcovia/mitigation/sat/cnf/BaseFormula.java
+++ bundles/dev.arcovia.mitigation.sat/src/dev/arcovia/mitigation/sat/cnf/BaseFormula.java
@@ -1,5 +1,10 @@
 package dev.arcovia.mitigation.sat.cnf;
 
+import dev.arcovia.mitigation.sat.Constraint;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BaseFormula {
 	protected final LogicNode root;
 	
@@ -7,7 +12,12 @@ public class BaseFormula {
 		this.root = root;
 	}
 	
-	public void toCNF() {
-		
+	public List<Constraint> toCNF() {
+        var constraint = new Constraint(new ArrayList<>());
+        var result = new ArrayList<>(List.of(constraint));
+        var activeClauses = new ArrayList<>(List.of(constraint));
+
+		root.collectCNFClauses(result, activeClauses);
+        return result;
 	}
 }
--- bundles/dev.arcovia.mitigation.sat/src/dev/arcovia/mitigation/sat/cnf/CNFTranslation.java
+++ bundles/dev.arcovia.mitigation.sat/src/dev/arcovia/mitigation/sat/cnf/CNFTranslation.java
@@ -4,144 +4,91 @@ import java.util.ArrayList;
 
 import org.dataflowanalysis.analysis.DataFlowConfidentialityAnalysis;
 import org.dataflowanalysis.analysis.dsl.AnalysisConstraint;
-import org.dataflowanalysis.analysis.dsl.selectors.AbstractSelector;
-import org.dataflowanalysis.analysis.dsl.selectors.CharacteristicsSelectorData;
-import org.dataflowanalysis.analysis.dsl.selectors.DataCharacteristicListSelector;
-import org.dataflowanalysis.analysis.dsl.selectors.DataCharacteristicsSelector;
+import org.dataflowanalysis.analysis.dsl.selectors.*;
 
 import dev.arcovia.mitigation.sat.Constraint;
-import dev.arcovia.mitigation.sat.IncomingDataLabel;
-import dev.arcovia.mitigation.sat.Label;
-import dev.arcovia.mitigation.sat.Literal;
 
 public class CNFTranslation {
 	protected final AnalysisConstraint analysisConstraint;
 	protected DataFlowConfidentialityAnalysis analysis;
 	protected ArrayList<Constraint> cnf;
-	protected BaseFormula dataFormula;
-	protected BaseFormula nodeFormula;
-	protected BaseFormula variableFormula;
-	
+    protected BaseFormula baseFormula;
+
 	public CNFTranslation(AnalysisConstraint analysisConstraint) {
 		this.analysisConstraint = analysisConstraint;
 	}
-	
-	//TODO check for vertexSourceSelectors
-	
-	private void constructDataFormula() {
-		var root = new ConjunctionNode();
-		dataFormula = new BaseFormula(root);
-		
-		var selectors = analysisConstraint.getDataSourceSelectors().getSelectors();
-		
-		for (AbstractSelector selector: selectors) {
-			// Java 17 implementation for pattern matching syntax of class instance, later version enable this with switch
-			
-			if (selector instanceof DataCharacteristicsSelector dcs) {
-				var type = dcs.getDataCharacteristic().characteristicType();
-				var value = dcs.getDataCharacteristic().characteristicValue();
-				
-				if(!type.isConstant()) throw new IllegalStateException("Selector Type not constant:" + selector);
-				if(!value.isConstant()) throw new IllegalStateException("Selector Value not constant:" + selector); // maybe this is for WHERE
-				
-				root.addPredicate(
-						new LiteralNode(
-								new Literal(
-										!dcs.isInverted(), // this gets inverted once for the resulting CNF
-										new IncomingDataLabel(
-												new Label(
-													type.values().get().get(0),
-													value.values().get().get(0)
-														)
-												)
-										)
-								)
-						);
-				continue;
-			}
-			
-			if (selector instanceof DataCharacteristicListSelector dcls) {
-				// expecting a LIST(OR) like ( a OR b OR ...)
-				var disjunctionNode = new DisjunctionNode();
-				root.addPredicate(disjunctionNode);
-				
-				for (CharacteristicsSelectorData csd: dcls.getDataCharacteristic()) {
-					
-					var type = csd.characteristicType();
-					var value = csd.characteristicValue();
-					
-					if(!type.isConstant()) throw new IllegalStateException("Selector Type not constant:" + selector);
-					if(!value.isConstant()) throw new IllegalStateException("Selector Value not constant:" + selector); // maybe this is for WHERE
-					
-					disjunctionNode.addPredicate(
-							new LiteralNode(
-									new Literal(
-											dcls.isInverted(), // this gets double inverted, once for NOT ( a OR b ) and once for resulting CNF
-											new IncomingDataLabel(
-													new Label(
-														csd.characteristicType().toString(),
-														csd.characteristicValue().toString()
-															)
-													)
-											)
-									)
-							);
-					continue;
-				}
-			}
-			
-			throw new IllegalStateException("Unexpected type:" + selector);
-		}
-	}
-	
-	private void constructNodeFormula() {
-		var root = new ConjunctionNode();
-		nodeFormula = new BaseFormula(root);
-		
-		var selectors = analysisConstraint.getVertexDestinationSelectors().getSelectors();
-		
-		for (AbstractSelector selector: selectors) {
-			// Java 17 implementation for pattern matching syntax of class instance, later version enable this with switch
-			
-			if (selector instanceof DataCharacteristicsSelector dcs) {
-				root.addPredicate(
-						new LiteralNode(
-								new Literal(
-										!dcs.isInverted(), 
-										new IncomingDataLabel(
-												new Label(
-													dcs.getDataCharacteristic().characteristicType().toString(),
-													dcs.getDataCharacteristic().characteristicValue().toString()
-														)
-												)
-										)
-								)
-						);
-				continue;
-			}
-			if (selector instanceof DataCharacteristicListSelector dcls) {
-				var disjunctionNode = new DisjunctionNode();
-				for (CharacteristicsSelectorData csd: dcls.getDataCharacteristic())
-				disjunctionNode.addPredicate(
-						new LiteralNode(
-								new Literal(
-										!dcls.isInverted(), 
-										new IncomingDataLabel(
-												new Label(
-													csd.characteristicType().toString(),
-													csd.characteristicValue().toString()
-														)
-												)
-										)
-								)
-						);
-				root.addPredicate(disjunctionNode);
-				continue;
-			}
-			
-			throw new IllegalStateException("Unexpected type:" + selector);
-		}
-	}
+
+    private void constructBaseFormula() {
+        var root = new ConjunctionNode();
+        baseFormula = new BaseFormula(root);
+
+        var hasOutgoingData = !analysisConstraint.getVertexSourceSelectors().getSelectors().isEmpty();
+        var selectors = new ArrayList<AbstractSelector>();
+        selectors.addAll(analysisConstraint.getDataSourceSelectors().getSelectors());
+        selectors.addAll(analysisConstraint.getVertexSourceSelectors().getSelectors());
+        selectors.addAll(analysisConstraint.getVertexDestinationSelectors().getSelectors());
+
+        for (AbstractSelector selector: selectors) {
+            // Java 17 implementation for pattern matching syntax of class instance, later version enable this with switch
+
+            if (selector instanceof DataCharacteristicsSelector dataCharacteristicsSelector) {
+                addDataCharacteristicSelector(root, dataCharacteristicsSelector, hasOutgoingData);
+                continue;
+            }
+
+            if (selector instanceof DataCharacteristicListSelector dataCharacteristicListSelector) {
+                addDataCharacteristicListSelector(root, dataCharacteristicListSelector, hasOutgoingData);
+                continue;
+            }
+
+            if (selector instanceof VertexCharacteristicsSelector vertexCharacteristicsSelector) {
+                addVertexCharacteristicsSelector(root, vertexCharacteristicsSelector);
+                continue;
+            }
+
+            if (selector instanceof VertexCharacteristicsListSelector vertexCharacteristicsListSelector) {
+                addVertexCharacteristicsListSelector(root, vertexCharacteristicsListSelector);
+                continue;
+            }
+
+            throw new IllegalStateException("Unexpected type:" + selector);
+        }
+    }
+
+    private void addDataCharacteristicSelector(BranchNode root, DataCharacteristicsSelector dataCharacteristicsSelector, boolean hasOutgoingData) {
+        root.addPredicate(LiteralNode.ofIncomingData(dataCharacteristicsSelector));
+        if (hasOutgoingData) {
+            root.addPredicate(LiteralNode.ofOutgoingData(dataCharacteristicsSelector));
+        }
+    }
+
+    private void addDataCharacteristicListSelector(BranchNode root, DataCharacteristicListSelector dataCharacteristicListSelector, boolean hasOutgoingData) {
+        var inverted = dataCharacteristicListSelector.isInverted();
+        var characteristicsSelectorDataList = dataCharacteristicListSelector.getCharacteristicsSelectorDataList();
+        var node = inverted ? new ConjunctionNode() : new DisjunctionNode();
+        root.addPredicate(node);
+
+        for (CharacteristicsSelectorData characteristicsSelectorData: characteristicsSelectorDataList) {
+            var dataCharacteristicsSelector = new DataCharacteristicsSelector(null, characteristicsSelectorData, inverted);
+            addDataCharacteristicSelector(node, dataCharacteristicsSelector, hasOutgoingData);
+        }
+    }
+
+    private void addVertexCharacteristicsSelector(BranchNode root, VertexCharacteristicsSelector vertexCharacteristicsSelector) {
+        root.addPredicate(LiteralNode.ofNode(vertexCharacteristicsSelector));
+    }
+
+    private void addVertexCharacteristicsListSelector(BranchNode root, VertexCharacteristicsListSelector vertexCharacteristicsListSelector) {
+        var inverted = vertexCharacteristicsListSelector.isInverted();
+        var characteristicsSelectorDataList = vertexCharacteristicsListSelector.getCharacteristicsSelectorDataList();
+        var node = inverted ? new ConjunctionNode() : new DisjunctionNode();
+        root.addPredicate(node);
+
+        for (CharacteristicsSelectorData characteristicsSelectorData: characteristicsSelectorDataList) {
+            var vertexCharacteristicsSelector = new VertexCharacteristicsSelector(null, characteristicsSelectorData, inverted);
+            addVertexCharacteristicsSelector(node, vertexCharacteristicsSelector);
+        }
+    }
 	
 	private void constructVariableFormula() {
 		
