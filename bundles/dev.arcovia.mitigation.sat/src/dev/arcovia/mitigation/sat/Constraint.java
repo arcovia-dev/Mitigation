38fa1db6833b274d4ea1e3b7b8cb5d8eb496235e
jensii <jens.weber@uni-ulm.de>
2025-08-19 22:51:53 +0200

    removed comment

--- bundles/dev.arcovia.mitigation.sat/src/dev/arcovia/mitigation/sat/Constraint.java
+++ bundles/dev.arcovia.mitigation.sat/src/dev/arcovia/mitigation/sat/Constraint.java
@@ -9,21 +9,4 @@ import java.util.List;
  * a specific logical condition or rule. Each literal within the constraint is a
  * component that can either be satisfied or not, based on its state.
  */
-public record Constraint(List<Literal> literals) {
-
-    // TODO remove after debugging
-    @Override
-    @NonNull
-    public String toString() {
-        var s = new StringBuilder();
-            for (var literal : literals) {
-                if (literals.contains(literal)) {
-                    s.append(literal.positive() ? "" : "!").append(literals.indexOf(literal)).append(" ");
-                } else {
-                    s.append(literal.positive() ? "" : "!").append(literals.size()).append(" ");
-                    literals.add(literal);
-                }
-            }
-        return s.toString();
-    }
-}
+public record Constraint(List<Literal> literals) {}
