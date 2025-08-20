20e73268b92ed92dc5ca4fcd3623717b6c44d827
jensii <jens.weber@uni-ulm.de>
2025-08-20 13:00:00 +0200

    added Tests

--- tests/dev.arcovia.mitigation.sat.tests/src/dev/arcovia/mitigation/sat/cnf/tests/utility/CNFUtil.java
+++ tests/dev.arcovia.mitigation.sat.tests/src/dev/arcovia/mitigation/sat/cnf/tests/utility/CNFUtil.java
@@ -19,7 +19,17 @@ public abstract class CNFUtil {
                 constraint.add(new Literal(!it.positive(), new OutgoingDataLabel(new Label(LabelCategory.OutgoingData.name(), it.value())))));
         nodes.forEach(it ->
                 constraint.add(new Literal(!it.positive(), new NodeLabel(new Label(LabelCategory.Node.name(), it.value())))));
+        return new Constraint(constraint);
+    }
 
+    public static Constraint generateNodeClause( List<DInData> incomingData, List<DOutData> outgoingData, List<DNode> nodes) {
+        ArrayList<Literal> constraint = new ArrayList<>();
+        nodes.forEach(it ->
+                constraint.add(new Literal(!it.positive(), new NodeLabel(new Label(LabelCategory.Node.name(), it.value())))));
+        incomingData.forEach(it ->
+                constraint.add(new Literal(!it.positive(), new IncomingDataLabel(new Label(LabelCategory.IncomingData.name(), it.value())))));
+        outgoingData.forEach(it ->
+                constraint.add(new Literal(!it.positive(), new OutgoingDataLabel(new Label(LabelCategory.OutgoingData.name(), it.value())))));
         return new Constraint(constraint);
     }
 
