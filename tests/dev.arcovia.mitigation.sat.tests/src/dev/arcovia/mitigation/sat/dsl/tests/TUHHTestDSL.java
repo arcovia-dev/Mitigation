8a33704b0299a8d532d0a08f2527da0a2e37b84f
jensii <jens.weber@uni-ulm.de>
2025-10-01 13:45:00 +0200

    used getter of TUHHTest for constraints

--- tests/dev.arcovia.mitigation.sat.tests/src/dev/arcovia/mitigation/sat/dsl/tests/TUHHTestDSL.java
+++ tests/dev.arcovia.mitigation.sat.tests/src/dev/arcovia/mitigation/sat/dsl/tests/TUHHTestDSL.java
@@ -3,6 +3,7 @@ package dev.arcovia.mitigation.sat.dsl.tests;
 import dev.arcovia.mitigation.sat.*;
 import dev.arcovia.mitigation.sat.dsl.CNFTranslation;
 import dev.arcovia.mitigation.sat.dsl.tests.utility.CNFUtil;
+import dev.arcovia.mitigation.sat.tests.TUHHTest;
 import org.apache.log4j.Logger;
 import org.dataflowanalysis.analysis.dsl.AnalysisConstraint;
 import org.dataflowanalysis.analysis.dsl.constraint.ConstraintDSL;
@@ -17,33 +18,7 @@ public class TUHHTestDSL {
 
     private final Logger logger = Logger.getLogger(TUHHTestDSL.class);
 
-    final Constraint entryViaGatewayOnly = new Constraint(List.of(new Literal(false, new NodeLabel(new Label("Stereotype", "internal"))),
-            new Literal(false, new IncomingDataLabel(new Label("Stereotype", "entrypoint"))),
-            new Literal(true, new IncomingDataLabel(new Label("Stereotype", "gateway")))));
-    final Constraint nonInternalGateway = new Constraint(List.of(new Literal(false, new NodeLabel(new Label("Stereotype", "gateway"))),
-            new Literal(false, new NodeLabel(new Label("Stereotype", "internal")))));
-    final Constraint authenticatedRequest = new Constraint(List.of(new Literal(false, new NodeLabel(new Label("Stereotype", "internal"))),
-            new Literal(true, new IncomingDataLabel(new Label("Stereotype", "authenticated_request")))));
-    final Constraint transformedEntry = new Constraint(List.of(new Literal(false, new NodeLabel(new Label("Stereotype", "internal"))),
-            new Literal(false, new IncomingDataLabel(new Label("Stereotype", "entrypoint"))),
-            new Literal(true, new IncomingDataLabel(new Label("Stereotype", "transform_identity_representation")))));
-    final Constraint tokenValidation = new Constraint(List.of(new Literal(false, new NodeLabel(new Label("Stereotype", "internal"))),
-            new Literal(false, new IncomingDataLabel(new Label("Stereotype", "entrypoint"))),
-            new Literal(true, new IncomingDataLabel(new Label("Stereotype", "token_validation")))));
-    final Constraint loginAttempts = new Constraint(List.of(new Literal(false, new NodeLabel(new Label("Stereotype", "authorization_server"))),
-            new Literal(true, new NodeLabel(new Label("Stereotype", "login_attempts_regulation")))));
-    final Constraint encryptedEntry = new Constraint(List.of(new Literal(false, new IncomingDataLabel(new Label("Stereotype", "entrypoint"))),
-            new Literal(true, new IncomingDataLabel(new Label("Stereotype", "encrypted_connection")))));
-    final Constraint encryptedInternals = new Constraint(List.of(new Literal(false, new IncomingDataLabel(new Label("Stereotype", "internal"))),
-            new Literal(true, new IncomingDataLabel(new Label("Stereotype", "encrypted_connection")))));
-    final Constraint localLogging = new Constraint(List.of(new Literal(false, new NodeLabel(new Label("Stereotype", "internal"))),
-            new Literal(true, new NodeLabel(new Label("Stereotype", "local_logging")))));
-    final Constraint logSanitization = new Constraint(List.of(new Literal(false, new NodeLabel(new Label("Stereotype", "local_logging"))),
-            new Literal(true, new NodeLabel(new Label("Stereotype", "log_sanitization")))));
-    // List of TUHH constraints without existence checks like auth-,logging-,secrets-server
-
-    final List<Constraint> constraints = List.of(entryViaGatewayOnly, nonInternalGateway, authenticatedRequest, transformedEntry, tokenValidation,
-            loginAttempts, encryptedEntry, encryptedInternals, localLogging, logSanitization);
+    final List<Constraint> constraints = new TUHHTest().getConstraints();
 
     final List<AnalysisConstraint> analysisConstraints = List.of(
             new ConstraintDSL().ofData()
