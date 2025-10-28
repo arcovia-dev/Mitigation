6c455647472f1729e77de7e0d7e5dbdbd70e329b
BenjaminArp <101420246+BenjaminArp@users.noreply.github.com>
2025-10-28 17:50:48 +0100

    use default model from sat

--- tests/dev.arcovia.mitigation.ilp.tests/src/dev/arcovia/mitigation/ilp/tests/BasicTest.java
+++ tests/dev.arcovia.mitigation.ilp.tests/src/dev/arcovia/mitigation/ilp/tests/BasicTest.java
@@ -2,6 +2,8 @@ package dev.arcovia.mitigation.ilp.tests;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import org.dataflowanalysis.analysis.dsl.AnalysisConstraint;
@@ -12,7 +14,15 @@ import org.junit.jupiter.api.Test;
 import dev.arcovia.mitigation.ilp.OptimizationManager;
 
 public class BasicTest {
-    private final String MinDFD = "models/mindfd.json";
+	Path current = Paths.get(System.getProperty("user.dir"));
+
+    private final String MinDFD = current
+        .getParent() 
+        .resolve("dev.arcovia.mitigation.sat.tests") 
+        .resolve("models")
+        .resolve("minsat.json").toString();
+    
+   
     AnalysisConstraint constraint = new ConstraintDSL().ofData()
             .withLabel("Sensitivity", "Personal")
             .withoutLabel("Encryption", "Encrypted")
@@ -22,7 +32,7 @@ public class BasicTest {
             .create();
 
     @Test
-    public void minTest() {
+    public void minTest() {  	
         var optimization = new OptimizationManager(MinDFD, List.of(constraint));
 
         var result = optimization.repair();
