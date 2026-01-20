4db06cc33180e07a8bfb57fbb737a6805fe9c523
BenjaminArp <101420246+BenjaminArp@users.noreply.github.com>
2026-01-20 22:13:44 +0100

    removing dead code

--- bundles/dev.arcovia.mitigation.ilp/src/dev/arcovia/mitigation/ilp/OptimizationManager.java
+++ bundles/dev.arcovia.mitigation.ilp/src/dev/arcovia/mitigation/ilp/OptimizationManager.java
@@ -136,8 +136,6 @@ public class OptimizationManager {
     }
 
     private List<ActionTerm> getActions(List<Mitigation> result) {
-        List<Mitigation> additional = new ArrayList<>();        
-        result.addAll(additional);
         List<ActionTerm> actions = new ArrayList<>();
         for (var mit : result) {
             actions.add(mit.mitigation());
