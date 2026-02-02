e091e0921fda092c37a6c9a717c9ede0595f40fb
BenjaminArp <101420246+BenjaminArp@users.noreply.github.com>
2026-02-02 15:34:56 +0100

    first eval

--- bundles/dev.arcovia.mitigation.ilp/src/dev/arcovia/mitigation/ilp/timeMeasurement.java
+++ bundles/dev.arcovia.mitigation.ilp/src/dev/arcovia/mitigation/ilp/timeMeasurement.java
@@ -35,7 +35,7 @@ public class timeMeasurement {
     public long getSolvingTime() {
         return solving - dfdAnalysis;
     }
-    public long getIsolatedExecutio() {
+    public long getIsolatedExecution() {
         return endTime - dfdAnalysis + constraints - startTime;
     }
     
--- tests/dev.arcovia.mitigation.ilp.tests/src/dev/arcovia/mitigation/ilp/tests/PerformanceEval.java
+++ tests/dev.arcovia.mitigation.ilp.tests/src/dev/arcovia/mitigation/ilp/tests/PerformanceEval.java
@@ -5,17 +5,15 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Set;
-import java.util.concurrent.ThreadLocalRandom;
+
 import java.util.ArrayList;
-import java.util.HashSet;
 
 import org.dataflowanalysis.analysis.dsl.AnalysisConstraint;
 import org.dataflowanalysis.analysis.dsl.constraint.ConstraintDSL;
 import org.junit.jupiter.api.Test;
 
 import dev.arcovia.mitigation.ilp.OptimizationManager;
-import dev.arcovia.mitigation.sat.DFDScaler;
+import dev.arcovia.mitigation.sat.Scaler;
 import dev.arcovia.mitigation.ilp.timeMeasurement;
 
 public class PerformanceEval {
@@ -40,65 +38,190 @@ public class PerformanceEval {
     @Test
     public void scaleTFGLength() {
         List<Integer>  scalings = List.of(0,0,1,2,4,8,16,32,64,128,256,512);
-        List<Long>  results = new ArrayList<>();
+        List<Long>  resultsSolving = new ArrayList<>();
+        List<Long>  resultsExecution = new ArrayList<>();
+        List<Long>  resultsIsolatedExec = new ArrayList<>();
         
         for (var scaling : scalings) {
             timeMeasurement timer = new timeMeasurement();
             
-            var scaler = new DFDScaler(MinDFD);
+            var scaler = new Scaler(MinDFD);
             var scaledDFD = scaler.scaleTFGLength(scaling);
             
             var optimization = new OptimizationManager(scaledDFD, List.of(constraint));
             var dfd = optimization.repair(timer);
             
-            results.add(timer.getSolvingTime());
+            resultsSolving.add(timer.getSolvingTime());
+            resultsExecution.add(timer.getExecutionTime());
+            resultsIsolatedExec.add(timer.getIsolatedExecution());
             
             assertTrue(optimization.isViolationFree(dfd));
             
         }
-        System.out.println(results);
+        System.out.println(resultsSolving);
+        System.out.println(resultsExecution);
+        System.out.println(resultsIsolatedExec);
     }
     
     @Test
     public void scaleTFGAmount() {
-        List<Integer>  scalings = List.of(0,0,1,2,4,8,16,32,64,128,256,512,1024,2024, 10000, 100000);
+        List<Integer>  scalings = List.of(0,0,1,2,4,8,16,32,64,128,256,512,1024,2024, 10000);
         // results [860, 2, 2, 2, 1, 3, 2, 2, 3, 4, 5, 7, 5, 13, 63, 3692]
-        List<Long>  results = new ArrayList<>();
+        List<Long>  resultsSolving = new ArrayList<>();
+        List<Long>  resultsExecution = new ArrayList<>();
+        List<Long>  resultsIsolatedExec = new ArrayList<>();
         
         for (var scaling : scalings) {
             timeMeasurement timer = new timeMeasurement();
             
-            var scaler = new DFDScaler(MinDFD);
+            var scaler = new Scaler(MinDFD);
             var scaledDFD = scaler.scaleTFGAmount(scaling);
             
             var optimization = new OptimizationManager(scaledDFD, List.of(constraint));
             var dfd = optimization.repair(timer);
             
-            results.add(timer.getSolvingTime());
+            resultsSolving.add(timer.getSolvingTime());
+            resultsExecution.add(timer.getExecutionTime());
+            resultsIsolatedExec.add(timer.getIsolatedExecution());
             
             assertTrue(optimization.isViolationFree(dfd));        
             
         }
-        System.out.println(results);
+        System.out.println(resultsSolving);
+        System.out.println(resultsExecution);
+        System.out.println(resultsIsolatedExec);
     }
     
     @Test
     public void scaleConstraintLabel() {
-        timeMeasurement timer = new timeMeasurement();
         
-        var scaler = new DFDScaler(MinDFD);
-        var scaledDFD = scaler.scaleLabels(30);
         
-        var constraints = getScaledConstraint(5, 1, 1, 1, 30);
+        int numberDummyLabels = 400;
+        List<Integer>  scalings = List.of(1,10, 50, 100);
         
-        var optimization = new OptimizationManager(scaledDFD, constraints);
-        var dfd = optimization.repair(timer);
-       
+        List<ScalingResults> times = new ArrayList<>();
         
-        assertTrue(optimization.isViolationFree(dfd));  
+        for (int amountConstraint : scalings) {
+            timeMeasurement timer = new timeMeasurement();
+            var scaler = new Scaler(MinDFD);
+            var scaledDFD = scaler.scaleLabels(numberDummyLabels);
+            
+            
+            var constraints = scaler.scaleConstraint(amountConstraint, 1, 1
+                    , 1, 1, numberDummyLabels);        
+            var optimization = new OptimizationManager(scaledDFD, constraints);
+            var dfd = optimization.repair(timer);
+           
+            
+            assertTrue(optimization.isViolationFree(dfd));                     
+            
+            times.add(new ScalingResults(amountConstraint, 1, 1, 1, 1,timer.getExecutionTime(), timer.getSolvingTime(), timer.getIsolatedExecution()));
+        }
+        
+        for (int numberWithLabel : scalings) {
+            timeMeasurement timer = new timeMeasurement();
+            var scaler = new Scaler(MinDFD);
+            var scaledDFD = scaler.scaleLabels(numberDummyLabels);
+            
+            
+            var constraints = scaler.scaleConstraint(1, numberWithLabel, 1
+                    , 1, 1, numberDummyLabels);        
+            var optimization = new OptimizationManager(scaledDFD, constraints);
+            var dfd = optimization.repair(timer);
+           
+            
+            assertTrue(optimization.isViolationFree(dfd));                     
+            
+            times.add(new ScalingResults(1, numberWithLabel, 1
+                    , 1, 1,
+                    timer.getExecutionTime(), timer.getSolvingTime(), timer.getIsolatedExecution()));   
+        }
+        
+        for (int numberWithoutLabel : scalings) {
+            timeMeasurement timer = new timeMeasurement();
+            var scaler = new Scaler(MinDFD);
+            var scaledDFD = scaler.scaleLabels(numberDummyLabels);
+            
+            
+            var constraints = scaler.scaleConstraint(1, 1, numberWithoutLabel
+                    , 1, 1, numberDummyLabels);        
+            var optimization = new OptimizationManager(scaledDFD, constraints);
+            var dfd = optimization.repair(timer);
+           
+            
+            assertTrue(optimization.isViolationFree(dfd));                     
+            
+            times.add(new ScalingResults(1, 1, numberWithoutLabel
+                    , 1, 1,
+                    timer.getExecutionTime(), timer.getSolvingTime(), timer.getIsolatedExecution()));
+        }
+        
+        for (int numberWithCharacteristic : scalings) {
+            timeMeasurement timer = new timeMeasurement();
+            var scaler = new Scaler(MinDFD);
+            var scaledDFD = scaler.scaleLabels(numberDummyLabels);
+            
+            
+            var constraints = scaler.scaleConstraint(1, 1, 1
+                    , numberWithCharacteristic, 1, numberDummyLabels);        
+            var optimization = new OptimizationManager(scaledDFD, constraints);
+            var dfd = optimization.repair(timer);
+           
+            
+            assertTrue(optimization.isViolationFree(dfd));                     
+            
+            times.add(new ScalingResults(1, 1, 1
+                    , numberWithCharacteristic, 1,
+                    timer.getExecutionTime(), timer.getSolvingTime(), timer.getIsolatedExecution()));
+        }
+        
+        for (int numberWithoutCharacteristic : scalings) {
+            timeMeasurement timer = new timeMeasurement();
+            var scaler = new Scaler(MinDFD);
+            var scaledDFD = scaler.scaleLabels(numberDummyLabels);
+            
+            
+            var constraints = scaler.scaleConstraint(1, 1, 1
+                    , 1, numberWithoutCharacteristic, numberDummyLabels);        
+            var optimization = new OptimizationManager(scaledDFD, constraints);
+            var dfd = optimization.repair(timer);
+           
+            
+            assertTrue(optimization.isViolationFree(dfd));                     
+            
+            times.add(new ScalingResults(1, 1, 1
+                    , 1, numberWithoutCharacteristic,
+                    timer.getExecutionTime(), timer.getSolvingTime(), timer.getIsolatedExecution()));
+        }
+        
+        
+        for (int scaling : scalings) {
+            timeMeasurement timer = new timeMeasurement();
+            var scaler = new Scaler(MinDFD);
+            var scaledDFD = scaler.scaleLabels(numberDummyLabels);
+            
+            
+            var constraints = scaler.scaleConstraint(scaling, scaling/2, scaling/2
+                    , scaling/2, scaling/2, numberDummyLabels);        
+            var optimization = new OptimizationManager(scaledDFD, constraints);
+            var dfd = optimization.repair(timer);
+           
+            
+            assertTrue(optimization.isViolationFree(dfd));                     
+            
+            times.add(new ScalingResults(scaling, scaling/2, scaling/2
+                    , scaling/2, scaling/2,
+                    timer.getExecutionTime(), timer.getSolvingTime(), timer.getIsolatedExecution()));
+        }
+                
+        
+        System.out.println(times);
+        
     }
     
     
-    
+        private record ScalingResults(int amountConstraint, int numberWithLabel, int numberWithoutLabel
+                , int numberWithCharacteristic, int numberWithoutCharacteristic, long executionTime, long solvingTime, long IsolatedExecution) {
+    }
 
 }
