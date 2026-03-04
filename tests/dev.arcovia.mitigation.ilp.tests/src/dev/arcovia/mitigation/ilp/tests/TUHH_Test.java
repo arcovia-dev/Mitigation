43d4588cd9076d9221c8cce6d94950db96bba1ad
BenjaminArp <101420246+BenjaminArp@users.noreply.github.com>
2026-03-04 11:08:02 +0100

    remove cyclic check

--- tests/dev.arcovia.mitigation.ilp.tests/src/dev/arcovia/mitigation/ilp/tests/TUHH_Test.java
+++ tests/dev.arcovia.mitigation.ilp.tests/src/dev/arcovia/mitigation/ilp/tests/TUHH_Test.java
@@ -132,9 +132,9 @@ public class TUHH_Test {
                 dfdConverter.convert(result)
                         .save("models/", "temp-repaired.json");
 
-                if (!optimization.isCyclic(result)) {
-                    assertTrue(optimization.isViolationFree(result));
-                }
+
+                assertTrue(optimization.isViolationFree(result));
+                
             }
         }
         System.out.println(scalabilityValues);
